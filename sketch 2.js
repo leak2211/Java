8bf31e18e46819d471e5e417b39e0ff95a6aa105
leak2211 <144492917+leak2211@users.noreply.github.com>
2024-02-29 01:02:37 +0300

    Create sketch 2.js

---
+++ sketch 2.js
@@ -0,0 +1,57 @@
+function setup()
+{
+	//create a large square canvas
+	createCanvas(800, 800);
+}
+
+function draw()
+{
+
+	//set the fill colour to red
+	fill(200, 25, 25);
+
+	//set a thick stroke weight for the black lines
+	strokeWeight(12);
+
+	//draw the red rectangle 
+	rect(100, 50, 600, 600);
+
+	
+	fill(171, 65, 230);
+	strokeWeight(25);
+	rect(150, 300, 100, 100);
+
+	fill(250, 134, 222);
+	strokeWeight(8);
+	rect(200, 450, 134, 180);
+
+	fill(119, 60, 76);
+	strokeWeight(5);
+	rect(400, 144, 245, 150);
+
+	fill(202, 167, 252);
+	strokeWeight(7);
+	rect(300, 600, 299, 20);
+
+
+	fill(181, 109, 122);
+	strokeWeight(3);
+	rect(400, 500, 256, 25);
+
+	fill(129, 255, 153);
+	strokeWeight(6);
+	rect(365, 550, 256, 25);
+
+	fill(118, 227, 156);
+	strokeWeight(7);
+	rect(170, 90, 219, 157);
+
+	fill(171, 211, 222);
+	strokeWeight(3);
+	rect(550, 330, 35, 145);
+
+	fill(209, 181, 111);
+	strokeWeight(5);
+	rect(300, 340, 234, 95);
+
+}
